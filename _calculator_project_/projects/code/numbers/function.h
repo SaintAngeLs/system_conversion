6674c7343c23b1c7ef6b78b4d7b0a3b1b1d57ad8
SaintAngeLs <96334335+SaintAngeLs@users.noreply.github.com>
2023-04-27 00:59:40 +0200

    Delete _calculator_project_/projects/code directory

--- _calculator_project_/projects/code/numbers/function.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-char digitToHex(int N);
-int hexToDigit(char ch);
-long long convertToDecimalFrom2To10(long long a, int sys);
-long long convertToDecimalFrom11To16(char* hex, int sys);
-int correctNumSystem(char* str, int scale);
